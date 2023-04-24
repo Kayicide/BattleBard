53bdda56734590899b39ee1209bf40cc491aefd8
Scarlett <Scarlett@kayde.me>
2023-04-24 21:06:25 +0100

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
-import { useState } from "react";
-import { ExampleModal } from "~/components/modals/modal";
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   return (
